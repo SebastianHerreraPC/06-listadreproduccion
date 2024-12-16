62e93849ab099c1786287704111aee84f317cfd1
SebastianHerreraPC <sebehepe00@gmail.com>
2024-12-16 15:00:13 -0600

    cambios form

---
+++ app/index.js
@@ -0,0 +1,4 @@
+const form = document.querySelectorAll(".form");
+const title = document.querySelector(".title__input");
+const artist = document.querySelector(".artist__input");
+console.log(form);
